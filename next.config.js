44e585abc8e0e78186f71b79cac50f7606badb4c
Kyriakos-Bekas <kyriakosbekas00@gmail.com>
2022-09-19 13:58:36 +0300

    Configured next.config.js and removed vercel.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://demo-vegan-life.vercel.app',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,POST',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
