661dbfc629744b1b97d35117c20787f89019b154
Kyriakos-Bekas <kyriakosbekas00@gmail.com>
2022-09-19 14:11:32 +0300

    Reconfigured for email api

--- next.config.js
+++ next.config.js
@@ -2,23 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    async headers() {
-        return [
-            {
-                source: '/api/:path*',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://demo-vegan-life.vercel.app',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,POST',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
